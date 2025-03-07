dd69296611b87385e10ded8d24f92a9e96d85982
Mazen-Mamoun <mazenmamoun12@gmail.com>
2025-03-07 22:19:13 +0200

    Update app_router.dart

--- lib/helper/app_router.dart
+++ lib/helper/app_router.dart
@@ -1,16 +1,21 @@
 import 'package:go_router/go_router.dart';
+import 'package:tawasel/views/home_view.dart';
 import 'package:tawasel/views/learning_content_screen.dart';
 import 'package:tawasel/views/letters_content_screen.dart';
+import 'package:tawasel/views/login.dart';
 import 'package:tawasel/views/numbers_content_screen.dart';
 import 'package:tawasel/views/splash_view.dart';
 import 'package:tawasel/views/test_screen.dart';
+import 'package:tawasel/views/words_content_screen.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kSplashView = '/splashView';
+  static const kloginView = '/loginView';
   static const kLearningContentScreen = '/learningContentScreen';
   static const kLettersScreen = '/lettersScreen';
   static const kNumberScreen = '/numbersScreen';
+  static const kWordsScreen = '/wordsScreen';
   static const kTestScreen = '/testScreen';
   static final router = GoRouter(
     routes: [
@@ -21,6 +26,18 @@ abstract class AppRouter {
       GoRoute(
         path: kLearningContentScreen,
         builder: (context, state) => const LearningContentScreen(),
+      ),
+      GoRoute(
+        path: kloginView,
+        builder: (context, state) => login(),
+      ),
+      GoRoute(
+        path: kWordsScreen,
+        builder: (context, state) => WordsScreen(),
+      ),
+      GoRoute(
+        path: kHomeView,
+        builder: (context, state) => const HomeView(),
       ),
       GoRoute(
         path: kTestScreen,
