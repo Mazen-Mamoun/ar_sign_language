63e67e85c2dc6d815bdae7ed1a8de12dc6729679
ZiadAlaa955 <145320240+ZiadAlaa955@users.noreply.github.com>
2025-03-08 00:49:49 +0200

    add app_utils file

---
+++ lib/helper/app_utils.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+OutlineInputBorder customOutlineInputBorder() {
+  return OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.grey.shade100,
+    ),
+    borderRadius: BorderRadius.circular(25),
+  );
+}
