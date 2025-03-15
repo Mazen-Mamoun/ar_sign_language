f19d7323c4d2e73e888087364d9cf4336d02a1ac
Mazen-Mamoun <mazenmamoun12@gmail.com>
2025-03-15 14:17:16 +0200

    Add files via upload

---
+++ lib/views/exam_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:mafhom_app/widgets/content_app_bar.dart';
+import 'package:mafhom_app/widgets/content_section.dart';
+
+
+class ExamScreen extends StatelessWidget {
+  const ExamScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            const ContentAppBar(text: 'الإختبار'),
+            Expanded(child: ContentSection(id: "الإختبار",)),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/words_exam_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mafhom_app/widgets/content_app_bar.dart';
+import 'package:mafhom_app/widgets/words_exam_section.dart';
+
+class WordsExamScreen extends StatelessWidget {
+  WordsExamScreen({super.key});
+  final List<Map<String, String>> camItems = [
+    {'text': "- افتح الكاميرا وقم بتمثيل  كلمة (مرحبا):"},
+    {'text': "- افتح الكاميرا وقم بتمثيل  الحرف (يشرب):"},
+    {'text': "- افتح الكاميرا وقم بتمثيل  الحرف (ينام):"},
+    {'text': "- افتح الكاميرا وقم بتمثيل  الحرف (يأكل):"},
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: ContentAppBar(text: 'الكلمات'),
+
+      body: WordsExamSection(camItems: camItems),
+    );
+  }
+}
+
