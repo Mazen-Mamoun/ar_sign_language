9cd77ebaa91f86a9c0634d5f25a0eae91a9601fb
tasneemgamal83 <54412709+tasneemgamal83@users.noreply.github.com>
2025-03-17 02:45:36 +0200

    Add files via upload

---
+++ lib/views/password_recovery_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sign_language/password/Password%20recovery.dart';
+
+class password_recovery_view extends StatelessWidget {
+  const password_recovery_view({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+     return
+     Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30,vertical: 20),
+          child:
+          Password_recovery(),
+        ),
+
+    );
+  }
+}
