b1d9d21a24f2ae90a28df9ca13e98750c5755ed5
ZiadAlaa955 <145320240+ZiadAlaa955@users.noreply.github.com>
2025-03-20 21:20:30 +0200

    Update edit_profile_card.dart

--- lib/widgets/edit_profile_card.dart
+++ lib/widgets/edit_profile_card.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:tawasel/widgets/edit_card_button.dart';
+import 'package:tawasel/widgets/edit_card_text_field.dart';
 
 class EditProfileCard extends StatelessWidget {
   const EditProfileCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return const Card(
       shadowColor: Colors.grey,
       elevation: 3,
       color: Colors.white,
       child: Padding(
-        padding: const EdgeInsets.all(20),
+        padding: EdgeInsets.all(20),
         child: Column(
           children: [
-            const Row(
+            Row(
               children: [
                 Icon(Icons.edit_note),
                 SizedBox(width: 10),
@@ -23,51 +25,18 @@ class EditProfileCard extends StatelessWidget {
                 ),
               ],
             ),
-            const SizedBox(height: 10),
-            Row(
-              children: [
-                const Text(
-                  'اسم المستخدم:',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                const Spacer(),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  height: 45,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[100],
-                      border: const OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                //  const Spacer(),
-              ],
+            SizedBox(height: 10),
+            EditCardTextField(
+              title: 'اسم المستخدم:',
+              textFieldData: 'Ziadalaa955@gmail.com',
             ),
-            const SizedBox(height: 10),
-            Row(
-              children: [
-                const Text(
-                  'كلمة المرور:',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(width: MediaQuery.of(context).size.width * 0.05),
-                const Spacer(),
-                SizedBox(
-                  height: 45,
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[100],
-                      border: const OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                //const Spacer(),
-              ],
+            SizedBox(height: 10),
+            EditCardTextField(
+              title: 'كلمة المرور:',
+              textFieldData: '123465789',
             ),
+            SizedBox(height: 15),
+            EditCardButton(),
           ],
         ),
       ),
