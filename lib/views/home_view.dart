d296ae78694fe0d7c45252347ffc1bac7c7fa878
ZiadAlaa955 <145320240+ZiadAlaa955@users.noreply.github.com>
2025-03-20 21:00:44 +0200

    Update home_view.dart

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -5,57 +5,25 @@ import 'package:sign_app/Widgets/home_body.dart';
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
-  final String userName = 'زياد علاء';
-
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: Scaffold(
-        body: SafeArea(
+    return const Scaffold(
+      body: Directionality(
+        textDirection: TextDirection.rtl,
+        child: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 15),
+            padding: EdgeInsets.symmetric(vertical: 18, horizontal: 15),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                HomeAppBar(userName: userName),
-                const SizedBox(height: 80),
-                const HomeBody(),
+                HomeAppBar(),
+                SizedBox(height: 20),
+                HomeBody(),
               ],
             ),
           ),
         ),
       ),
+      bottomNavigationBar: MainBottomNavigationBar(currentIndex: 2),
     );
   }
 }
-//Container(
-//   decoration: BoxDecoration(
-//     color: kPrimaryColor,
-//     borderRadius: BorderRadius.circular(16),
-//   ),
-//   child: const Padding(
-//     padding: EdgeInsets.all(12),
-//     child: Column(
-//       crossAxisAlignment: CrossAxisAlignment.start,
-//       children: [
-//         Text(
-//           'مرحباٌ بك في تطبيق التعليم ',
-//           style: TextStyle(
-//             color: Colors.white,
-//             fontWeight: FontWeight.bold,
-//             fontSize: 20,
-//           ),
-//         ),
-//         Text(
-//           'استكشف المحتوي التعليمي وترجم النصوص والفيديوهات ',
-//           style: TextStyle(
-//             color: Colors.white,
-//             fontWeight: FontWeight.w500,
-//             fontSize: 16,
-//           ),
-//         ),
-//       ],
-//     ),
-//   ),
-// ),
