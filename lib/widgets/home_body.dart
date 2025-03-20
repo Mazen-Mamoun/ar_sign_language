3e8cf826eb7270e602aa46662ab405e5afeb89b2
ZiadAlaa955 <145320240+ZiadAlaa955@users.noreply.github.com>
2025-03-20 21:21:58 +0200

    Update home_body.dart

--- lib/widgets/home_body.dart
+++ lib/widgets/home_body.dart
@@ -1,50 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:sign_app/Widgets/home_buttons_row.dart';
-import 'package:sign_app/helper/app_router.dart';
+import 'package:tawasel/helper/app_router.dart';
+import 'package:tawasel/widgets/home_card_item.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        const Text(
-          'ترجمة',
-          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 50),
-        ),
-        const SizedBox(height: 10),
-        HomeButtonsRow(
-          leftButtonTitle: 'نص',
-          rightButtonTitle: 'فيديو',
-          leftButtonIcon: Icons.notes,
-          rightButtonIcon: Icons.videocam,
-          leftOnTap: () {
+    final List<HomeCardItem> homeCardItems = [
+      HomeCardItem(
+          title: 'ترجمة فيديو',
+          icon: Icons.videocam,
+          onTap: () {
             GoRouter.of(context).push(AppRouter.kTestScreen);
-          },
-          rightOnTap: () {
+          }),
+      HomeCardItem(
+          title: 'ترجمة نص',
+          icon: Icons.notes,
+          onTap: () {
             GoRouter.of(context).push(AppRouter.kTestScreen);
-          },
-        ),
-        const SizedBox(height: 20),
-        const Text(
-          'تعليم',
-          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 50),
-        ),
-        const SizedBox(height: 10),
-        HomeButtonsRow(
-          leftButtonTitle: 'المحتوي',
-          rightButtonTitle: 'الاختبار',
-          leftButtonIcon: Icons.description,
-          rightButtonIcon: Icons.check_box,
-          leftOnTap: () {
+          }),
+      HomeCardItem(
+          title: 'المحتوي التعليمي',
+          icon: Icons.description,
+          onTap: () {
             GoRouter.of(context).push(AppRouter.kLearningContentScreen);
-          },
-          rightOnTap: () {
-            GoRouter.of(context).push(AppRouter.kTestScreen);
-          },
+          }),
+      HomeCardItem(
+          title: 'اختبارات',
+          icon: Icons.check_box,
+          onTap: () {
+            GoRouter.of(context).push(AppRouter.kExamScreen);
+          }),
+    ];
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Divider(indent: 10, endIndent: 10),
+        Text(
+          'ترجمه',
+          style: TextStyle(
+            color: Colors.grey[700],
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            homeCardItems[0],
+            homeCardItems[1],
+          ],
+        ),
+        const SizedBox(height: 10),
+        Text(
+          'تعليم',
+          style: TextStyle(
+            color: Colors.grey[700],
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            homeCardItems[2],
+            homeCardItems[3],
+          ],
         ),
       ],
     );
