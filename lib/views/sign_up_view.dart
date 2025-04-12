2f028325588641d380b493d8dcc614b71eb8dffe
ZiadAlaa955 <145320240+ZiadAlaa955@users.noreply.github.com>
2025-04-12 15:14:15 +0200

    Update sign_up_view.dart

--- lib/views/sign_up_view.dart
+++ lib/views/sign_up_view.dart
@@ -1,19 +1,19 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sign_language/signUp.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tawasel/Cubits/auth_cubit/auth_cubit.dart';
+import 'package:tawasel/widgets/auth_widgets/sign_up_body.dart';
 
-class sign_up_view extends StatelessWidget {
-  const sign_up_view({Key? key}) : super(key: key);
+class SignUpView extends StatelessWidget {
+  const SignUpView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Directionality(
+    return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30),
-          child: signUp(),
+        body: BlocProvider(
+          create: (context) => AuthCubit(),
+          child: const SignUpBody(),
         ),
       ),
     );
