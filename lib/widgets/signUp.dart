504bb0b9678ac4bab0857e9d83141426505f6cd8
ZiadAlaa955 <145320240+ZiadAlaa955@users.noreply.github.com>
2025-04-12 15:18:02 +0200

    Update signUp.dart

--- lib/widgets/signUp.dart
+++ lib/widgets/signUp.dart
@@ -1,70 +1,152 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sign_language/login.dart';
-import 'package:sign_language/loginin_navigatore.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:tawasel/Cubits/auth_cubit/auth_cubit.dart';
+import 'package:tawasel/helper/app_router.dart';
+import 'package:tawasel/helper/app_utils.dart';
+import 'package:tawasel/widgets/auth_widgets/auth_navigation_text.dart';
+import 'package:tawasel/widgets/auth_widgets/custom_auth_button.dart';
+import 'package:tawasel/widgets/auth_widgets/log_in_body.dart';
+import 'package:tawasel/widgets/custom_text_form_field.dart';
 
-import 'CustomTextFormField.dart';
+class SignUpBody extends StatefulWidget {
+  const SignUpBody({Key? key}) : super(key: key);
 
-class signUp extends StatelessWidget {
-   signUp({Key? key}) : super(key: key);
+  @override
+  State<SignUpBody> createState() => _SignUpBodyState();
+}
+
+class _SignUpBodyState extends State<SignUpBody> {
   final GlobalKey<FormState> form = GlobalKey();
+
+  bool isPasswordVisible = true;
+  bool isConfirmPasswordVisible = true;
+  String? userName, email, password, confirmPassword;
+  bool isLodaing = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-    body:
-      Form(
-      key: form,
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-            Image.asset("asset/image/register.png",height: 250,),
-            const SizedBox(height: 20),
-            const CustomTextFormField(
-              title: 'اسم المستخدم',
-              preficIcon: Icons.person,
-              validatorText: 'ادخل اسم المستخدم الخاص بك',
-            ),
-            const SizedBox(height: 15),
-            const CustomTextFormField(
-              title: 'البريد الالكتروني',
-              preficIcon: Icons.email_outlined,
-              validatorText: 'ادخل البريد الالكتروني الخاص بك',
-            ),
-            const SizedBox(height: 15,),
-            const CustomTextFormField(
-              title: 'كلمة السر',
-              preficIcon: Icons.lock_outlined,
-              suffixIcon: Icons.visibility,
-              validatorText: 'ادخل كلمة السر الخاصة بك',
-            ),
-            const SizedBox(height: 15,),
-            const CustomTextFormField(
-              title: 'تاكيد كلمة السر',
-              preficIcon: Icons.lock_outlined,
-              suffixIcon: Icons.visibility,
-              validatorText: 'ادخل كلمة السر الخاصة بك',
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 48,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xff648DDC)
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthLoading) {
+          isLodaing = true;
+        } else if (state is AuthFailure) {
+          isLodaing = false;
+
+          snackBar(context, state.errorMessage);
+        } else if (state is AuthSuccess) {
+          isLodaing = false;
+
+          userData = state.authApiSuccessData;
+
+          snackBar(context, state.authApiSuccessData.message);
+
+          GoRouter.of(context).push(AppRouter.kHomeView);
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+          inAsyncCall: isLodaing,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Form(
+              key: form,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                    Image.asset("assets/images/sign_up_image.png", height: 250),
+                    CustomTextFormField(
+                      onChanged: (name) {
+                        userName = name;
+                      },
+                      obscureText: false,
+                      title: 'اسم المستخدم',
+                      prefixIcon: Icons.person,
+                      validatorText: 'ادخل اسم المستخدم الخاص بك',
+                    ),
+                    const SizedBox(height: 15),
+                    CustomTextFormField(
+                      onChanged: (mail) {
+                        email = mail;
+                      },
+                      obscureText: false,
+                      title: 'البريد الالكتروني',
+                      prefixIcon: Icons.email_outlined,
+                      validatorText: 'ادخل البريد الالكتروني الخاص بك',
+                    ),
+                    const SizedBox(height: 15),
+                    CustomTextFormField(
+                      onChanged: (pass) {
+                        password = pass;
+                      },
+                      obscureText: !isPasswordVisible,
+                      title: 'كلمة السر',
+                      prefixIcon: Icons.lock_outlined,
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          isPasswordVisible = !isPasswordVisible;
+                          setState(() {});
+                        },
+                        icon: isPasswordVisible
+                            ? const Icon(Icons.visibility)
+                            : const Icon(Icons.visibility_off),
+                        color: Colors.grey.shade500,
+                      ),
+                      validatorText: 'ادخل كلمة السر الخاصة بك',
+                    ),
+                    const SizedBox(height: 15),
+                    CustomTextFormField(
+                      onChanged: (pass) {
+                        confirmPassword = pass;
+                      },
+                      obscureText: !isConfirmPasswordVisible,
+                      title: 'تاكيد كلمة السر',
+                      prefixIcon: Icons.lock_outlined,
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          isConfirmPasswordVisible = !isConfirmPasswordVisible;
+                          setState(() {});
+                        },
+                        icon: isConfirmPasswordVisible
+                            ? const Icon(Icons.visibility)
+                            : const Icon(Icons.visibility_off),
+                        color: Colors.grey.shade500,
+                      ),
+                      validatorText: 'ادخل كلمة السر الخاصة بك',
+                    ),
+                    const SizedBox(height: 20),
+                    CusotmAuthButton(
+                      form: form,
+                      onPressed: () async {
+                        if (form.currentState!.validate()) {
+                          BlocProvider.of<AuthCubit>(context).registerUser(
+                            name: userName!,
+                            email: email!,
+                            password: password!,
+                            confirmPassword: confirmPassword!,
+                          );
+                          FocusScope.of(context).unfocus();
+                        }
+                      },
+                      title: 'انشاء حساب',
+                    ),
+                    const SizedBox(height: 10),
+                    AuthNavigationText(
+                      text: 'لديك حساب بالفعل',
+                      textButton: 'تسجيل الدخول',
+                      onTap: () {
+                        GoRouter.of(context).pop();
+                      },
+                    ),
+                  ],
                 ),
-                  onPressed: (){}, child: Text("انشاء حساب",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),)),
+              ),
             ),
-            SizedBox(
-              height: 20,
-            ),
-            loginin_navigator(),
-            const SizedBox(height: 10),
-          ],
-        ),
-      ),));
+          ),
+        );
+      },
+    );
   }
 }
