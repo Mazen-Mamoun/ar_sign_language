dd1227b849626db1aabe13754b20135c737f066c
ZiadAlaa955 <145320240+ZiadAlaa955@users.noreply.github.com>
2025-04-12 15:13:39 +0200

    Update login_view.dart

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:tawasel/widgets/sign_in_body.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tawasel/Cubits/auth_cubit/auth_cubit.dart';
+import 'package:tawasel/widgets/auth_widgets/log_in_body.dart';
 
 class LogInView extends StatelessWidget {
   const LogInView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Directionality(
+    return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30),
-          child: LogInBody(),
+        body: BlocProvider(
+          create: (context) => AuthCubit(),
+          child: const LogInBody(),
         ),
       ),
     );
