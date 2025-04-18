40f6e79084fd6e4cdb8688dd7fd06e4d503fb34d
tasneemgamal83 <54412709+tasneemgamal83@users.noreply.github.com>
2025-04-19 01:16:59 +0200

    Add files via upload

--- lib/widgets/verification_code.dart
+++ lib/widgets/verification_code.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sign_language/password/password_recovery_view.dart';
 import 'package:pinput/pinput.dart';
+import 'package:sign_language/password/reset%20password_view.dart';
 
 class verification_code extends StatelessWidget {
    verification_code({Key? key}) : super(key: key);
@@ -25,68 +26,79 @@ class verification_code extends StatelessWidget {
     }, ),)
     ),
 
-    body: Form(child:
-    SingleChildScrollView(
-    child:Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-    children: [
-    SizedBox(
-    height: 15,
-    ),
-    Text("رمز التحقق",textAlign: TextAlign.left,style: TextStyle(
-    color: Colors.black,
-    fontSize: 20,
-    fontWeight: FontWeight.bold,
-    )),
-    SizedBox(
-      height: 16,
-    ),
-    Text("..لقد أرسلنا رمز التحقق إلى بريدك الإلكترونى",style: TextStyle(
-    color: Color.fromRGBO(152, 152, 152, 1),
-    fontSize: 14,
-    fontWeight: FontWeight.bold,
-    ),),
-      Text("...من فضلك أدخل الرمز",style: TextStyle(
-        color: Color.fromRGBO(152, 152, 152, 1),
-        fontSize: 14,
-        fontWeight: FontWeight.bold,
-      ),),
-    SizedBox(height: 40,),
-     Center(
-       child: Pinput(
-           defaultPinTheme: PinTheme(
-             width: 60,
-             height: 60,
-             textStyle: TextStyle(
-               fontSize: 20,
-               color: Colors.black,
-               fontWeight: FontWeight.w200,
-             ),
-             decoration: BoxDecoration(
-               color: Colors.grey.shade100,
-               borderRadius: BorderRadius.all(Radius.circular(13)),
-             )
-           ),
-         length: 5,
-       ),
-     )
-,
-      SizedBox(
-        height: 30,
-      ),
-      Center(
-        child: SizedBox(
-          width: 390,
-          height: 48,
-          child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xff648DDC),
-              ),
-              onPressed: (){
-              }, child: Text("تاكيد رمز ",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),)),
-        ),
-      ),
-    ]
-    ),),),);
+        body: Form(
+          child: SingleChildScrollView(
+                  child:Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                    SizedBox(
+                    height: 15,
+                    ),
+                    Text("رمز التحقق",textAlign: TextAlign.left,style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    )),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Text("..لقد أرسلنا رمز التحقق إلى بريدك الإلكترونى",style: TextStyle(
+                    color: Color.fromRGBO(152, 152, 152, 1),
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    ),),
+                      Text("...من فضلك أدخل الرمز",style: TextStyle(
+                        color: Color.fromRGBO(152, 152, 152, 1),
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    SizedBox(height: 40,),
+                     Center(
+                       child:
+                           Pinput(
+                               defaultPinTheme: PinTheme(
+                                 width: 60,
+                                 height: 60,
+                                 textStyle: TextStyle(
+                                   fontSize: 20,
+                                   color: Colors.black,
+                                   fontWeight: FontWeight.w200,
+                                 ),
+                                 decoration: BoxDecoration(
+                                   color: Colors.grey.shade100,
+                                   borderRadius: BorderRadius.all(Radius.circular(13)),
+                                 )
+                               ),
+                             length: 5,
+                           ),
+                       ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Center(
+                        child: SizedBox(
+                          width: 390,
+                          height: 48,
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(Color(0xff648DDC)),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  // Change your radius here
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                              ),
+
+
+                            ),
+                            onPressed: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=>reset_password_view()));
+                            }, child: Text("تاكيد رمز ",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),
+                          ),
+                          ),
+                        ),
+                      ),
+                    ],),
+                    ),),);
   }
 }
