c231947c3e19c2e7ee3b84369f54e7b9e54078df
Mazen-Mamoun <mazenmamoun12@gmail.com>
2025-04-27 21:06:51 +0300

    Update words_content_screen.dart

--- lib/views/words_content_screen.dart
+++ lib/views/words_content_screen.dart
@@ -3,21 +3,19 @@ import 'package:tawasel/widgets/content_app_bar.dart';
 
 import '../widgets/sign_content.dart';
 
-class WordsScreen extends StatelessWidget {
-  WordsScreen({super.key});
-  final List<Map<String, String>> wordItems = [
-    {'mediaPath': 'assets/numbers_video.mp4', 'text': 'تجربة'},
-    {'mediaPath': 'assets/numbers_video.mp4', 'text': 'تجربة'},
-    {'mediaPath': 'assets/numbers_video.mp4', 'text': 'تجربة'},
-    {'mediaPath': 'assets/numbers_video.mp4', 'text': 'تجربة'},
-  ];
+class WordsContentScreen extends StatelessWidget {
+  const WordsContentScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const ContentAppBar(text: "الكلمات"),
+    return const Scaffold(
+      appBar: ContentAppBar(text: "الكلمات"),
       body: Padding(
-        padding: const EdgeInsets.only(top: 0, right: 16, left: 16, bottom: 8),
-        child: SignContent(itemsCount: 1, items: wordItems),
+        padding: EdgeInsets.only(top: 0, right: 16, left: 16, bottom: 8),
+        child: SignContent(
+          itemsCount: 1,
+          categoryName: 'word',
+        ),
       ),
     );
   }
