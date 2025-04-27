5de473d662ecc780714d208eb6a89aead1727bfa
Mazen-Mamoun <mazenmamoun12@gmail.com>
2025-04-27 21:00:55 +0300

    Update Api.dart
    Add get method

--- lib/helper/Api.dart
+++ lib/helper/Api.dart
@@ -16,4 +16,23 @@ class Api {
 
     return jsonData;
   }
+
+  
+  Future<dynamic> get({
+    required String url,
+    required Map<String, String> headers,
+  }) async {
+    final Response response = await dio.get(
+      url,
+      options: Options(headers: headers),
+    );
+    if (response.statusCode == 200) {
+      // print(response.data);
+      return response.data;
+    } else {
+      throw Exception(
+        'there is a problem with status code ${response.statusCode}',
+      );
+    }
+  }
 }
