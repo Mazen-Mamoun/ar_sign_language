42898ae32629c5d19a8cbccbdc6e9881c0c24d41
Mazen-Mamoun <mazenmamoun12@gmail.com>
2025-04-27 21:06:03 +0300

    Update numbers_content_screen.dart

--- lib/views/numbers_content_screen.dart
+++ lib/views/numbers_content_screen.dart
@@ -1,30 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:tawasel/widgets/custom_app_bar.dart';
+import 'package:tawasel/widgets/content_app_bar.dart';
 
 import '../widgets/sign_content.dart';
 
 class NumbersScreen extends StatelessWidget {
-  NumbersScreen({super.key});
-  final List<Map<String, String>> numberItems = [
-    {'mediaPath': 'assets/images/logo.png', 'text': "1"},
-    {'mediaPath': 'assets/images/logo.png', 'text': "2"},
-    {'mediaPath': 'assets/images/logo.png', 'text': "3"},
-    {'mediaPath': 'assets/images/logo.png', 'text': "4"},
-    {'mediaPath': 'assets/images/logo.png', 'text': "4"},
-    {'mediaPath': 'assets/images/logo.png', 'text': "4"},
-    {'mediaPath': 'assets/images/logo.png', 'text': "4"},
-    {'mediaPath': 'assets/images/logo.png', 'text': "4"},
-  ];
+ const NumbersScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Directionality(
+    return const Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
-        appBar: const ContentAppBar(text: "الأرقام"),
+        appBar:  ContentAppBar(text: "الأرقام"),
         body: Padding(
           padding:
-              const EdgeInsets.only(top: 8, right: 16, left: 16, bottom: 8),
-          child: SignContent(itemsCount: 2, items: numberItems),
+               EdgeInsets.only(top: 8, right: 16, left: 16, bottom: 8),
+          child: SignContent(itemsCount: 2,  categoryName: 'number',),
         ),
       ),
     );
