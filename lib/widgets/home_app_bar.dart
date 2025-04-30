334b4034689eddee3b43db19c8f52669247375e1
ZiadAlaa955 <145320240+ZiadAlaa955@users.noreply.github.com>
2025-04-30 15:42:08 +0300

    Update home_app_bar.dart

--- lib/widgets/home_app_bar.dart
+++ lib/widgets/home_app_bar.dart
@@ -1,7 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tawasel/Cubits/auth_cubit/auth_cubit.dart';
 import 'package:tawasel/Models/auth_models/auth_api_success_response_model.dart';
-import 'package:tawasel/Models/auth_models/user_model.dart';
-import 'package:tawasel/widgets/auth_widgets/log_in_body.dart';
 import 'package:tawasel/widgets/profile_icon.dart';
 
 class HomeAppBar extends StatelessWidget {
@@ -9,28 +11,45 @@ class HomeAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'مرحبا بك،',
-              style: TextStyle(
-                color: Colors.grey[700],
-                fontSize: 30,
-                fontWeight: FontWeight.w600,
+    AuthApiSuccessResponse? userData;
+
+    return BlocBuilder<AuthCubit, AuthState>(
+      builder: (context, state) {
+        if (state is AuthSuccess) {
+          userData = state.authApiSuccessData;
+          log(
+            'userData in HomeViewCubit :\nname=>${userData!.user.name}\nemail=>${userData!.user.email}\ntoken=>${userData!.token}',
+          );
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'مرحبا بك،',
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const ProfileIcon(),
+                ],
               ),
-            ),
-            const ProfileIcon(),
-          ],
-        ),
-        Text(
-          userData!.user.name,
-          style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        ),
-      ],
+              Text(
+                userData!.user.name,
+                style:
+                    const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ],
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
