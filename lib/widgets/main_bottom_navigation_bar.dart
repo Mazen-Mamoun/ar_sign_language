919612ede254def07b1fd36950a18dea5333d929
ZiadAlaa955 <145320240+ZiadAlaa955@users.noreply.github.com>
2025-05-03 13:20:56 +0300

    Update main_bottom_navigation_bar.dart

--- lib/widgets/main_bottom_navigation_bar.dart
+++ lib/widgets/main_bottom_navigation_bar.dart
@@ -1,36 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:tawasel/Models/auth_models/auth_api_success_response_model.dart';
+import 'package:tawasel/helper/constants.dart';
 import '../helper/app_router.dart';
 
 class MainBottomNavigationBar extends StatelessWidget {
-  final int currentIndex;
+  const MainBottomNavigationBar({
+    super.key,
+    required this.currentIndex,
+  });
 
-  const MainBottomNavigationBar({super.key, required this.currentIndex});
+  final int currentIndex;
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
+      type: BottomNavigationBarType.shifting,
       currentIndex: currentIndex,
-      selectedItemColor: Colors.blue,
+      selectedItemColor: kPrimaryColor,
       unselectedItemColor: Colors.grey,
       onTap: (index) {
         switch (index) {
-          // case 0:
-          //   context.go(AppRouter.kHomeScreen);
-          //   break;
+          case 0:
+            context.push(AppRouter.kVideoTranselateView);
+            break;
           // case 1:
           //   context.go(AppRouter.kAlphaScreen);
           //   break;
-          // case 2:
-          //   context.go(AppRouter.kNumberScreen);
-          //   break;
-          case 3:
-            context.go(AppRouter.kLearningContentScreen);
+          case 2:
+            GoRouter.of(context).push(AppRouter.kHomeView);
+            // context.go(AppRouter.kHomeView);
             break;
-          // case 4:
-          //   context.go(AppRouter.kProfileScreen);
-          //   break;
+          case 3:
+            GoRouter.of(context).push(AppRouter.kLearningContentScreen);
+            // context.go(AppRouter.kLearningContentScreen);
+            break;
+          case 4:
+            GoRouter.of(context).push(AppRouter.kProfileView);
+            // context.go(AppRouter.kProfileView);
+            break;
         }
       },
       items: const [
