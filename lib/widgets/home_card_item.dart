a9812c90715e53bc42250814f5b9dc6d4626c9f4
ZiadAlaa955 <145320240+ZiadAlaa955@users.noreply.github.com>
2025-05-13 17:55:00 +0300

    Update home_card_item.dart

--- lib/widgets/home_card_item.dart
+++ lib/widgets/home_card_item.dart
@@ -7,11 +7,13 @@ class HomeCardItem extends StatelessWidget {
     required this.title,
     this.onTap,
     required this.icon,
+    required this.width,
   });
 
   final String title;
   final Function()? onTap;
   final IconData icon;
+  final double? width;
 
   @override
   Widget build(BuildContext context) {
@@ -19,21 +21,33 @@ class HomeCardItem extends StatelessWidget {
       onTap: onTap,
       child: SizedBox(
         height: MediaQuery.of(context).size.height * 0.2,
-        width: MediaQuery.of(context).size.width * 0.4,
+        width: width,
         child: Card(
-          elevation: 20,
+          elevation: 8,
           shadowColor: const Color(0xff374E7A),
           color: kPrimaryColor,
           child: Column(
             children: [
               const Spacer(flex: 2),
-              Icon(icon, size: 60, color: Colors.white),
+              Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Icon(
+                      icon,
+                      size: 45,
+                      color: Colors.white,
+                    ),
+                  )),
               const Spacer(),
               Padding(
                 padding: EdgeInsets.symmetric(
                     horizontal: MediaQuery.of(context).size.width * 0.02),
                 child: Container(
-                  width: double.infinity,
+                  width: 150,
                   decoration: BoxDecoration(
                     color: const Color(0xff374E7A),
                     borderRadius: BorderRadius.circular(8),
