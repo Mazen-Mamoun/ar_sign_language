8597241fa53fc70744874bd602b9c462501eddd3
Mazen-Mamoun <mazenmamoun12@gmail.com>
2025-05-28 14:46:42 +0300

    Update video_translate_cubit.dart

--- lib/cubits/video_translate_cubit/video_translate_cubit.dart
+++ lib/cubits/video_translate_cubit/video_translate_cubit.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:typed_data';
+
 import 'package:bloc/bloc.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'video_translate_state.dart';
@@ -8,8 +14,14 @@ class VideoTranslateCubit extends Cubit<VideoTranslateState> {
   VideoTranslateCubit() : super(VideoTranslateInitial());
 
   late CameraController controller;
+  Timer? _frameTimer;
+  bool _cameraActive = false;
+  bool _isSending = false;
 
-  Future<void> initializeCamera(List<CameraDescription> cameras) async {
+  Future<void> initializeCamera(
+    List<CameraDescription> cameras,
+    void Function(Uint8List imageBytes) onFrameCaptured,
+  ) async {
     final frontCamera = cameras[1];
 
     controller = CameraController(
@@ -20,10 +32,44 @@ class VideoTranslateCubit extends Cubit<VideoTranslateState> {
     await controller.initialize();
 
     emit(VideoTranslateCameraOn(controller));
+    _startSendingFrames(onFrameCaptured);
+  }
+
+  void _startSendingFrames(void Function(Uint8List) onFrameCaptured) {
+    _cameraActive = true;
+    _frameTimer = Timer.periodic(const Duration(seconds: 2), (_) async {
+      if (!_cameraActive ||
+          !controller.value.isInitialized ||
+          isClosed ||
+          _isSending) return;
+
+      try {
+        _isSending = true;
+
+        final image = await controller.takePicture();
+        final bytes = await image.readAsBytes();
+
+        onFrameCaptured(
+            bytes); // This triggers sendImage() in ApiTranslateCubit
+
+        // Delay after capturing to give time for response display
+        await Future.delayed(const Duration(seconds: 2));
+      } catch (e) {
+        log('Error capturing frame: $e');
+      } finally {
+        _isSending = false;
+      }
+    });
   }
 
   void stopCamera() {
+    _cameraActive = false;
     controller.dispose();
-    emit(VideoTranslateCameraOff());
+    _frameTimer?.cancel();
+
+    if (!isClosed) {
+      emit(VideoTranslateCameraOff());
+    }
   }
 }
+
