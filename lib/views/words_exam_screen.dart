d2bc46f7925c1d64052b60a9a8789b70264664c4
ZiadAlaa955 <145320240+ZiadAlaa955@users.noreply.github.com>
2025-06-02 01:34:24 +0300

    Update words_exam_screen.dart

--- lib/views/words_exam_screen.dart
+++ lib/views/words_exam_screen.dart
@@ -3,17 +3,18 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tawasel/Cubits/exam_cubit/exam_cubit.dart';
 import 'package:tawasel/Cubits/submit_cubit/submit_cubit.dart';
 import 'package:tawasel/widgets/content_widgets/content_app_bar.dart';
-import 'package:tawasel/widgets/exam_widgets/video_mcq_section.dart';
+import 'package:tawasel/widgets/exam_widgets/mcq_section.dart';
 
 class WordsExamScreen extends StatelessWidget {
   const WordsExamScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     BlocProvider.of<ExamCubit>(context).getexam(categoryId: 5);
 
     return Scaffold(
-      appBar: const ContentAppBar(text: ' الكلمات'),
+      appBar: const ContentAppBar(text: 'الكلمات'),
       body: BlocBuilder<ExamCubit, ExamState>(
         builder: (context, state) {
           if (state is ExamLoading) {
@@ -26,7 +27,7 @@ class WordsExamScreen extends StatelessWidget {
                     context: context,
                     builder: (context) => AlertDialog(
                       title: const Text('النتيجة'),
-                      content: Text('درجتك: ${state.score.toString()}'),
+                      content: Text('درجتك: ${state.score}'),
                       actions: [
                         TextButton(
                           onPressed: () => Navigator.of(context)
@@ -42,7 +43,29 @@ class WordsExamScreen extends StatelessWidget {
                   );
                 }
               },
-              child: VideoMcqSection(items: state.questions),
+
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'اختر الكلمة الصحيحة بناءً على الإشارة',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        McqSection(items: state.questions),
+                        const SizedBox(height: 30),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             );
           } else if (state is ExamError) {
             return Center(child: Text(state.message));
