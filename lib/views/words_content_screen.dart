736b40e2b0a4662d3cb1062e175ce31817480153
Mazen-Mamoun <mazenmamoun12@gmail.com>
2025-06-02 01:33:45 +0300

    Update words_content_screen.dart

--- lib/views/words_content_screen.dart
+++ lib/views/words_content_screen.dart
@@ -13,7 +13,7 @@ class WordsContentScreen extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.only(top: 0, right: 16, left: 16, bottom: 8),
         child: SignContent(
-         crossAxisCount: 1,
+         crossAxisCount: 2,
           categoryName: 'word',
         ),
       ),
